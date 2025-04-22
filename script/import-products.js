fd027563c2a0b580ace86c2a1b4453fc8dfda05f
Vemabhargav <bhargavasatyasai.vema@slu.edu>
2025-04-22 09:12:45 +0000

    complete lab 05

--- script/import-products.js
+++ script/import-products.js
@@ -1,11 +1,18 @@
-const db = require('../db');
-const Products = require('../products');
-
-const products = require('../data/full-products.json');
+const db = require('../db')
+const Products = require('../products')
+const products = require('../data/full-products.json')
 
 ;(async () => {
-  // loop over the products and create them
-  for (let i = 0; i < products.length; i++) {
-    console.log( await Products.create(products[i]));
+  try {
+    for (let i = 0; i < products.length; i++) {
+      const created = await Products.create(products[i])
+      console.log(`Imported: ${created._id}`)
+    }
+
+    console.log('All products imported successfully.')
+    process.exit(0)
+  } catch (err) {
+    console.error('Failed to import products:', err)
+    process.exit(1)
   }
 })()
